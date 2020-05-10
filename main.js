69eb8bc85ab5e45db81d49645f97df33f8d1ed7a
Igor Bandura <igor.bandura@o2.pl>
2020-05-10 19:12:27 +0200

    first changes of homepage

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 const firstName='Igor';
 const age= 28;
-console.log(`Cześć! mam na imię ${firstName} i mam ${age}lat. To moja pierwsza ssstrona!`)
+console.log(`Cześć! mam na imię ${firstName} i mam ${age}lat. To moja pierwsza ssstrona!`);
+const space= document.querySelector('.empty-space--js');
+space.innerHTML = 'treść wstrzyknięta przy użyciu JS'
